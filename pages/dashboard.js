944dfe7fa60bf62f73ce4dca4f1a4f9fbc1782bd
RickRyan26 <rickryan26@gmail.com>
2020-11-22 18:34:16 -0800

    test

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,12 +7,9 @@ import EmptyState from '@/components/EmptyState';
 const Dashboard = () => {
   const auth = useAuth();
 
-  console.log(2); // This does execute.
   if (!auth.user) {
-    console.log(3); // This does execute.
     return 'Loading...';
   }
-  console.log(1); //this doesn't as expected.
   return <EmptyState />; // But then this one does?
 };
 
